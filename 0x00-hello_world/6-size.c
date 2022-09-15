7a9c70f28d0af7effc00d9841b76e0f25f1355cc
Dustin Davis <davisdustin328@gmail.com>
2022-09-15 11:51:32 -0700

    different type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-  char charType;
-  int intType;
-  long int longType;
-  long long int longlongType;
-  float floatType;
+char charType;
+int intType;
+long int longType;
+long long int longlongType;
+float floatType;
 
-  printf("Size of a char: %zu bytes\n", sizeof(charType));
-  printf("Size of an int: %zu bytes\n", sizeof(intType));
-  printf("Size of a long int: %zu bytes\n", sizeof(longType));
-  printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
-  printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
   
-  return 0;
+return 0;
 }
