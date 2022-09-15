584f426c6818d68fcdfcc4aa088040ebf60cbef1
Dustin Davis <davisdustin328@gmail.com>
2022-09-15 11:32:39 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * printf - Display message in terminal
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
