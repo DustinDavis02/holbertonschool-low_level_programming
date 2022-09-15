93f88b8efe72a2d7fbbd44fc88131996c702f2cf
Dustin Davis <davisdustin328@gmail.com>
2022-09-15 09:46:31 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-/*
+/**
  * main - displays message in terminal
  *
  * return 0 if succesful
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
