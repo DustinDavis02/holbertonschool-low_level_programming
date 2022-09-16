f8df03ebf8a8a2e1b6fc6e2cfbf638214f3aa636
Dustin Davis <davisdustin328@gmail.com>
2022-09-16 08:20:51 -0700

    Print alaphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Displays message in terminal
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+
+char x = ('a' <= 'z'; ++x)
+putchar(x);
+return (0);
+
+}
