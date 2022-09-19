abec63ddd2bfcf39f26de605c4d015530cff8864
Dustin Davis <davisdustin328@gmail.com>
2022-09-19 11:53:17 -0700

    display alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+*
+*
+*
+*
+*/
+
+int print_alphabet_x10(void)
+{
+  int c;
+  for (c = 'a'; c <= 'z';c++)
+    {
+      _putchar("%c  ", c);
+}
+  _putchar('\n');
+  return: (0);
+}
