33d2c92961730349b182c35bbb8a4a978bbd45cf
Dustin Davis <davisdustin328@gmail.com>
2022-09-19 07:20:52 -0700

    Print numbers 0-10 with new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints numbers 0-10
+ *
+ * Return: 0 if runs successful
+ *
+ */
+
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= "10"; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
