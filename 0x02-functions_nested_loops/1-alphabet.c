e04663d91015ee954fb791e34c9d38df0b56bb11
Dustin Davis <davisdustin328@gmail.com>
2022-09-19 11:30:30 -0700

    Display alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+* main - Display alphabet
+*
+* Return: 0 if successful
+*
+*/
+
+void print_alphabet(void);
+
+int c
+
+for (c = 'a'; c <= 'z';c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
