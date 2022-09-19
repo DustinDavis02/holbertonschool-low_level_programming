1a3be8074f8a949e6e12c7bebbc72a4f4c035cdb
Dustin Davis <davisdustin328@gmail.com>
2022-09-19 08:06:27 -0700

    alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - Displays alphabet in reverse
+*
+* Return: 0 if successful
+*
+*/
+
+int main(void)
+{
+int i;
+for(i = 'z'; i = 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
