9df7a36f0df503437ecfe6ad1e1f20dda66ce830
Dustin Davis <davisdustin328@gmail.com>
2022-09-20 09:32:40 -0700

    code to check if char is in alphabet

---
+++ 0x02-functions_nested_loops/#3-islower.c#
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+* _islower - checks for lowercase
+* @c: The unknown character of the user.
+* Return: 0 if lowercase return 1 if not.
+*
+*/
+
+int _islower(int c)
+{
+int q;
+
+for (q = 'a'; q <= 'z'; q++)
+{
+if (c == q)
+return (1);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+*
+* _isalpha - Checks if a character is part of alphabet
+* @c: The unknown character the user selects.
+* Return: 0 if character is in alphabet and 1 if not.
+*
+*/
+
+int _isalpha(int c)
+{
+int q;
+
+for (q = 'a'; q <= 'z'; q++)
+for (q = 'A'; q <= 'Z'; q++)
+{
+if (c == q)
+return (1);
+}
+return (0);
+}
