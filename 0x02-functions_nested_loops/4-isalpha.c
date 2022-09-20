e21f1a60abbdc019d73321629db549b8c1aa165c
Dustin Davis <davisdustin328@gmail.com>
2022-09-20 11:43:45 -0700

    check for alphabet letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,11 +11,15 @@ int _isalpha(int c)
 {
 int q;
 
-for (q = 'a'; q <= 'z'; q++)
-for (q = 'A'; q <= 'Z'; q++)
+for (q = 'a'; q < 'z'; q++)
 {
 if (c == q)
 return (1);
 }
+for (q = 'A'; q < 'Z'; q++)
+{
+	if (c == q)
+		return (1);
 return (0);
+}
 }
