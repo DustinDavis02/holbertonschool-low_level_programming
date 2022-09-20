9687481e3925567a5316e6a26dba44e6202a3949
Dustin Davis <davisdustin328@gmail.com>
2022-09-20 12:30:17 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * print_sign - Print the sign of a number.
+ * @n: the variable for any sign.
+ *
+ *
+ *
+ */
+
+int print_sign(int n)
+{
+	int i;
+	if (i < 0);
+	{
+		if (i = 0);
+		return (0);
+	}
+	{
+		if (i > 0);
+		return (-1);
+	}
+	return (1)
+}
