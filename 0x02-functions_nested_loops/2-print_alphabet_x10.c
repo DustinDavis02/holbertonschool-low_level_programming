523eb0a955c64915480344cec7e90da2dd9b77ce
Dustin Davis <davisdustin328@gmail.com>
2022-09-20 07:35:01 -0700

    Print alphabet 10 times.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include"main.h"
 /**
+* print_alphabet_x10 - Displays alphabet 10 times
 *
-*
-*
+* Return: 0 if successful
 *
 */
 
 int print_alphabet_x10(void)
 {
-  int c;
-  for (c = 'a'; c <= 'z';c++)
-    {
-      _putchar("%c  ", c);
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar("abcdefghijklmnopqrstuvwxyz");
 }
-  _putchar('\n');
-  return: (0);
+_putchar('\n');
+return (0);
 }
