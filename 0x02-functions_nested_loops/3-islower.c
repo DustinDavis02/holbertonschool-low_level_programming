2b15cbfe40a39e54c6adfd72f7d6db1d90db1fbe
Dustin Davis <davisdustin328@gmail.com>
2022-09-20 08:29:47 -0700

    Check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _islower - checks for lowercase
+ *
+ * Return: 0 if successful
+ * 
+ */
+
+int _islower(int c)
+{
+c = _islower('H');
+_putchar(r + '0');
+c = _islower('z');
+_putchar(r + '0');
+return (0);
+}
