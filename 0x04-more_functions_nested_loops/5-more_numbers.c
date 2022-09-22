ae6368f8f3953c1a94cc71e50c54c8ca3bcc2fbc
Dustin Davis <davisdustin328@gmail.com>
2022-09-22 12:02:45 -0700

    Print number 0 - 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,23 @@
  */
 
 void more_numbers(void)
-
 {
-	int i;
-	int c;
 
-	i = 0;
-	while (i < 14)
+	int i = 0;
+	int c = 0;
+
+	while (c <= 9)
+	{
+		while (i <= 14)
 		{
-			for (c = 0; c <= 14; c++)
-			{
-				_putchar(c);
-			}
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			i++;
-			_putchar('\n');
+
 		}
+	_putchar('\n');
+	c++;
+	i = 0;
+	}
 }
