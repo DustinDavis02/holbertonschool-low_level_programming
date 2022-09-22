d6b603af1636c79b13070a8cfb452f5b8f8396e6
Dustin Davis <davisdustin328@gmail.com>
2022-09-22 08:29:03 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * mul - Multiply 2 numbers
+ * @a: - first unsigned variable
+ * @b: - second unsigned variable
+ * Return: The Result of the numbers.
+ */
+
+int mul(int a, int b){
+	return (x * y);
+}
