a87d31c15a44e9b8cd53604969afff1c64c8371d
Dustin Davis <davisdustin328@gmail.com>
2022-09-22 07:36:38 -0700

    files and uppercase check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *
+ * _isupper - Checks unsigned character for uppercase
+ *
+ * Return: 1 if C is uppercase other wise 0.
+ */
+
+int _isupper(int c)
+{
+	if (c == 'C')
+		return (1);
+	else
+		return (0);
+}
