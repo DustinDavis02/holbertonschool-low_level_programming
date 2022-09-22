bdfbdcb87a6e07785a1e65575937f77a39d476ad
Dustin Davis <davisdustin328@gmail.com>
2022-09-22 09:01:30 -0700

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_numbers - Displays 0-9
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+			}
+	_putchar('\n');
+}
