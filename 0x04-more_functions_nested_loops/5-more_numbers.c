d4b8601d8cb5be1a1f78bd18f56462bd7768c93a
Dustin Davis <davisdustin328@gmail.com>
2022-09-22 11:53:42 -0700

    Print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * more_numbers - prints number 0-14 ten times
+ *
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+
+{
+	int i;
+	int c;
+
+	i = 0;
+	while i < 14
+		{
+			for (c = 0; c <= 14; c++)
+			{
+				_putchar(c);
+			}
+			i++;
+			_putchar('\n');
+		}
+}
