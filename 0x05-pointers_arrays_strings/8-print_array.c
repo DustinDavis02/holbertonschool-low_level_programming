80b8d849bffde8717af683d486d0684f028e286f
Dustin Davis <davisdustin328@gmail.com>
2022-09-27 17:03:46 -0700

    fixing 5, Every other character, second half of string, and copy string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include  "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints array at n elements
+ *@a: array to print
+ *@n: number of elements 
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", *a);
+			break;
+		}
+		printf("%d, ", *a);
+		a++;
+	}
+	printf("\n");
+}
