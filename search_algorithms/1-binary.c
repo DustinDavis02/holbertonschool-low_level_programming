6105195084510c26c817aef150fa6da28d957602
DustinDavis02 <106279708+DustinDavis02@users.noreply.github.com>
2023-08-23 20:48:41 -0500

    Update 1-binary.c

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "search_algos.h"
+
+int binary_search(int *array, size_t size, int value)
+{
+    int low, high, mid;
+    size_t i;
+    if (array == NULL)
+        return -1;
+
+    low = 0;
+    high = size - 1;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        printf("Searching in array: ");
+        for (i = (size_t)low; i <= (size_t)high; i++)
+        {
+            printf("%d", array[i]);
+            if (i != (size_t)high)
+                printf(", ");
+        }
+        printf("\n");
+
+        if (array[mid] == value)
+            return mid;
+        else if (array[mid] < value)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1; /* Value not found */
+}
